a5efa52fe84434b7e540126b8dffc47e296da712
Jade of Spiders <flyingarmadillo@gmail.com>
2018-05-17 16:01:38 -0700

    master piece

--- src/Faithless.js
+++ src/Faithless.js
@@ -1,20 +1,63 @@
 import React, { Component } from 'react';
 
-
-function handleClick(e) {
-  console.log("ummm");
-}
-
 class Faithless extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {clicked: false};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState(prevstate => ({
+      clicked: true
+    }));
+  }
 
   render() {
-    return (
-        <p className="Faithless" onClick={handleClick}>faith | less</p>
-    );
+    if (this.state.clicked) {
+      return (
+        <p className="Faithless"><Master /> <Piece /></p>
+      );
+    }
+    else {
+      return (
+        <p className="Faithless" onClick={this.handleClick}>masterpiece</p>
+      );
+    }
   }
 
 
+}
+
+class Master extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {clicked: false};
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    this.setState(prevstate => ({
+      clicked: true
+    }));
+  }
+
+  render() {
+    return (
+      <span>master</span>
+    );
+  }
+}
+
+class Piece extends Component {
+  render() {
+    return (
+      <span>piece</span>
+    );
+  }
 }
 
 export default Faithless;
